7403da6b77938f3b14442b070c1c2fea25881564
Dawson-Bruce <brucedj@mail.uc.edu>
2024-09-20 01:37:04 -0400

    Completed Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args)
     {
-        
+        double purchasePrice = 58.40;
+        double salesTax = purchasePrice * .05;
+
+        System.out.println("The sales tax on a $" + purchasePrice + " purchase is $" + salesTax + ".");
     }
 }
